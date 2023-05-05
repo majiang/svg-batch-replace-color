37a8c67d2b002a5d8b0e1683ec1146ed65bc5085
majiang <majiang@users.noreply.github.com>
2023-05-05 10:54:18 +0900

    prettier for project config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,23 @@
 import typescriptEslintParser from '@typescript-eslint/parser'
 import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
 
-export default
-[
+export default [
   {
-    files: ["**/*.ts"],
-    ignores: ["**/*.spec.ts"],
-    languageOptions:
-    {
+    files: ['**/*.ts'],
+    ignores: ['**/*.spec.ts'],
+    languageOptions: {
       parser: typescriptEslintParser,
-      parserOptions:
-      {
-        'project': 'tsconfig.json',
+      parserOptions: {
+        project: 'tsconfig.json',
       },
     },
-    plugins:
-    {
+    plugins: {
       '@typescript-eslint': typescriptEslintPlugin,
     },
-    rules:
-    {
-      ...typescriptEslintPlugin.configs["recommended"].rules,
-      ...typescriptEslintPlugin.configs["recommended-requiring-type-checking"].rules,
+    rules: {
+      ...typescriptEslintPlugin.configs['recommended'].rules,
+      ...typescriptEslintPlugin.configs['recommended-requiring-type-checking']
+        .rules,
     },
   },
 ]
